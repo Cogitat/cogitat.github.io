031dcb68ea0f058c1488e79c1e0e7e2c990a152b
Levon Lloyd <levon@cogitat.co>
2017-01-02 09:14:41 -0500

    fix contact form submission

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function checkBeforeSubmit(){
   // add checks
-	document.getElementById('theForm').submit();
+	document.getElementById('GoogleForm').submit();
 	document.getElementById('success').hidden = false;
 }
